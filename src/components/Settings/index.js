b03ccd5c27a8bcea3c707328ded4ac392948b0c9
Rodrigo Ferreira <rodrigofepy@gmail.com>
2017-08-11 03:58:58 -0400

    feat: Update settings page to redirect to sign up page if the user is not authenticated

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,13 +1,25 @@
+import PropTypes from 'prop-types'
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 
 import Navigation from '../Navigation'
 
 import Wrapper from '../NavigationPagesWrapper'
 
-const Settings = () =>
-  <Wrapper>
-    <h1>Settings</h1>
-    <Navigation />
-  </Wrapper>
+const Settings = props => {
+  if (!props.authenticated) {
+    return <Redirect to="/sign-up" />
+  }
+  return (
+    <Wrapper>
+      <h1>Settings</h1>
+      <Navigation />
+    </Wrapper>
+  )
+}
+
+Settings.propTypes = {
+  authenticated: PropTypes.bool.isRequired
+}
 
 export default Settings
--- src/containers/SettingsPage/index.js
+++ src/containers/SettingsPage/index.js
@@ -1,7 +1,13 @@
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
+import makeSelectApp from '../App/selector'
 import Settings from '../../components/Settings'
 
-const SettingsPage = connect()(Settings)
+const mapStateToProps = createStructuredSelector({
+  authenticated: makeSelectApp('authenticated')
+})
+
+const SettingsPage = connect(mapStateToProps)(Settings)
 
 export default SettingsPage
