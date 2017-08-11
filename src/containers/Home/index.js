0a931981068aa05fcdd4260ef02fe0fe8d8adf0a
Gustavo <andrescabral.c@gmail.com>
2017-08-11 09:20:01 -0400

    feat: Initial files added

---
+++ src/containers/Home/index.js
@@ -0,0 +1,10 @@
+import { connect } from 'react-redux'
+
+import Home from '../../components/Home'
+
+const mapStateToProps = null
+const mapDispatchToProps = null
+
+const HomePage = connect(mapStateToProps, mapDispatchToProps)(Home)
+
+export default HomePage
