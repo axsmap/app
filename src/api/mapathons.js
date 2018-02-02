c983d6ced6d1275773c0cedb5b0b88183a52fbc9
Gustavo <andrescabral.c@gmail.com>
2018-02-02 11:11:26 -0300

    feat(mapathons api): add create mapathon endpoint

--- src/api/mapathons.js
+++ src/api/mapathons.js
@@ -3,3 +3,7 @@ import handleEndpoint from './handle-endpoint'
 export async function getMapathonsEndpoint(params) {
   return handleEndpoint({ method: 'get', url: '/events', params })
 }
+
+export async function createMapathonEndpoint(data) {
+  return handleEndpoint({ method: 'post', url: '/events', data })
+}
