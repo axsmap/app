f63ed8892b6ad881ff3714098fc292a667f2f4e4
Gustavo <andrescabral.c@gmail.com>
2018-03-17 20:32:32 -0300

    feat(components): add Contact files

---
+++ src/components/Contact/messages.js
@@ -0,0 +1,56 @@
+import { defineMessages } from 'react-intl'
+
+export default defineMessages({
+  pageTitle: {
+    id: 'axsmap.components.Contact.pageTitle',
+    defaultMessage: 'Contact Us | AXS Map'
+  },
+  headerTitle: {
+    id: 'axsmap.components.Contact.headerTitle',
+    defaultMessage: 'Contact us'
+  },
+  nameLabel: {
+    id: 'axsmap.components.Contact.nameLabel',
+    defaultMessage: 'Your name'
+  },
+  nameError1: {
+    id: 'axsmap.components.Contact.nameError1',
+    defaultMessage: 'Is required'
+  },
+  nameError2: {
+    id: 'axsmap.components.Contact.nameError2',
+    defaultMessage: 'Should be less than 61 characters'
+  },
+  emailLabel: {
+    id: 'axsmap.components.Contact.emailLabel',
+    defaultMessage: 'Your email'
+  },
+  emailError1: {
+    id: 'axsmap.components.Contact.emailError1',
+    defaultMessage: 'Is required'
+  },
+  emailError2: {
+    id: 'axsmap.components.Contact.emailError2',
+    defaultMessage: 'Should be less than 255 characters'
+  },
+  emailError3: {
+    id: 'axsmap.components.Contact.emailError3',
+    defaultMessage: 'Should be a valid email'
+  },
+  messageLabel: {
+    id: 'axsmap.components.Contact.messageLabel',
+    defaultMessage: 'Message'
+  },
+  messageError1: {
+    id: 'axsmap.components.Contact.messageError1',
+    defaultMessage: 'Is required'
+  },
+  messageError2: {
+    id: 'axsmap.components.Contact.messageError2',
+    defaultMessage: 'Should be less than 301 characters'
+  },
+  sendButton: {
+    id: 'axsmap.components.Contact.sendButton',
+    defaultMessage: 'Send'
+  }
+})
