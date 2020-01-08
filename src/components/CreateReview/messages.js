3c867537f900f1fd13787e9cd9145ba3f80c1f5d
Cinthya G Ligman <cinthya.g.ligman@pwc.com>
2020-01-08 11:28:45 -0500

    Updated messages per copy chanages

--- src/components/CreateReview/messages.js
+++ src/components/CreateReview/messages.js
@@ -13,6 +13,10 @@ export default defineMessages({
     id: 'axsmap.components.CreateReview.createReviewHeader',
     defaultMessage: 'Create review'
   },
+  createReviewSubheader: {
+    id: 'axsmap.components.CreateReview.createReviewSubheader',
+    defaultMessage: 'Does this location have…'
+  },
   commentsError: {
     id: 'axsmap.components.CreateReview.commentsError',
     defaultMessage: 'Should be less than 301 characters'
@@ -37,6 +41,195 @@ export default defineMessages({
     id: 'axsmap.components.CreateReview.entryTitle',
     defaultMessage: 'Entrance'
   },
+  permanentRampTitle: {
+    id: 'axsmap.components.CreateReview.permanentRampTitle',
+    defaultMessage: 'A permanent ramp'
+  },
+  permanentRampDescription: {
+    id: 'axsmap.components.CreateReview.permanentRampDescription',
+    defaultMessage:
+      'A ramp is an inclined plane installed in addition to or instead of stairs.'
+  },
+  portableRampTitle: {
+    id: 'axsmap.components.CreateReview.portableRampTitle',
+    defaultMessage: 'Portable ramp'
+  },
+  portableRampDescription: {
+    id: 'axsmap.components.CreateReview.portableRampDescription',
+    defaultMessage:
+      'A ramp is an inclined plane installed in addition to or instead of stairs.'
+  },
+  noStepsTitle: {
+    id: 'axsmap.components.CreateReview.noStepsTitle',
+    defaultMessage: 'No steps'
+  },
+  noStepsDescription: {
+    id: 'axsmap.components.CreateReview.noStepsDescription',
+    defaultMessage: 'The entrance to the venue has no steps.'
+  },
+  oneStepTitle: {
+    id: 'axsmap.components.CreateReview.oneStepTitle',
+    defaultMessage: 'One step'
+  },
+  oneStepDescription: {
+    id: 'axsmap.components.CreateReview.oneStepDescription',
+    defaultMessage:
+      'Any number of steps can seriously limit the possibility of easy navigation for wheelchairs, walkers, and other modes of mobility.'
+  },
+  twoStepsTitle: {
+    id: 'axsmap.components.CreateReview.twoStepsTitle',
+    defaultMessage: 'Two steps'
+  },
+  twoStepsDescription: {
+    id: 'axsmap.components.CreateReview.twoStepsDescription',
+    defaultMessage:
+      'Any number of steps can seriously limit the possibility of easy navigation for wheelchairs, walkers, and other modes of mobility.'
+  },
+  reservedParkingTitle: {
+    id: 'axsmap.components.CreateReview.reservedParkingTitle',
+    defaultMessage: 'Reserved parking'
+  },
+  reservedParkingDescription: {
+    id: 'axsmap.components.CreateReview.reservedParkingDescription',
+    defaultMessage:
+      'Reserved parking spots at least 96 in. wide with adjacent access aisles, and built on the shortest accessible route of travel from the point of parking to the venue entrance.'
+  },
+  wideEntranceTitle: {
+    id: 'axsmap.components.CreateReview.wideEntranceTitle',
+    defaultMessage: 'A wide entrance'
+  },
+  wideEntranceDescription: {
+    id: 'axsmap.components.CreateReview.wideEntranceDescription',
+    defaultMessage:
+      'For a wheelchair to comfortably fit through a doorway,  the doorway should be 32 inches wide. Commonly, doorways measure about 23 to 27 inches wide; this would not be wide enough for a wheelchair to fit through.'
+  },
+  roomToMoveTitle: {
+    id: 'axsmap.components.CreateReview.roomToMoveTitle',
+    defaultMessage: 'Room to move'
+  },
+  roomToMoveDescription: {
+    id: 'axsmap.components.CreateReview.roomToMoveDescription',
+    defaultMessage:
+      'The aisles between fixed seats must be at least 36 inches wide, and restaurants should provide wheelchair accessible seats throughout the dining room.'
+  },
+  accessibleTableHeightTitle: {
+    id: 'axsmap.components.CreateReview.accessibleTableHeightTitle',
+    defaultMessage: 'Accessible table height'
+  },
+  accessibleTableHeightDescription: {
+    id: 'axsmap.components.CreateReview.accessibleTableHeightDescription',
+    defaultMessage:
+      'The surface height should be no more than 34 inches and no less than 28 inches above the floor.'
+  },
+  brightLightTitle: {
+    id: 'axsmap.components.CreateReview.brightLightTitle',
+    defaultMessage: 'Bright light'
+  },
+  brightLightDescription: {
+    id: 'axsmap.components.CreateReview.brightLightDescription',
+    defaultMessage: 'Lorem Ipsum'
+  },
+  highNoiseLevelTitle: {
+    id: 'axsmap.components.CreateReview.highNoiseLevelTitle',
+    defaultMessage: 'High noise level'
+  },
+  highNoiseLevelDescription: {
+    id: 'axsmap.components.CreateReview.highNoiseLevelDescription',
+    defaultMessage: 'Lorem Ipsum'
+  },
+  guideDogTitle: {
+    id: 'axsmap.components.CreateReview.guideDogTitle',
+    defaultMessage: 'Guide dogs'
+  },
+  guideDogDescription: {
+    id: 'axsmap.components.CreateReview.guideDogDescription',
+    defaultMessage: 'Lorem Ipsum'
+  },
+  interiorRampTitle: {
+    id: 'axsmap.components.CreateReview.interiorRampTitle',
+    defaultMessage: 'Interior Ramp'
+  },
+  interiorRampDescription: {
+    id: 'axsmap.components.CreateReview.interiorRampDescription',
+    defaultMessage:
+      'A route inside of a building which slopes measurably from one flat surface to another. The ramp must be at least 36 in. wide.'
+  },
+  accessibleElevatorTitle: {
+    id: 'axsmap.components.CreateReview.accessibleElevatorTitle',
+    defaultMessage: 'Accessible elevator'
+  },
+  accessibleElevatorDescription: {
+    id: 'axsmap.components.CreateReview.accessibleElevatorDescription',
+    defaultMessage:
+      'Elevator doors should measure at least 36 inches, the depth of the elevator 51, and the width at least 68. The entrance and exit to and from the elevator must be readily accessible.'
+  },
+  doorSwingsInTitle: {
+    id: 'axsmap.components.CreateReview.doorSwingsInTitle',
+    defaultMessage: 'Door swings in'
+  },
+  doorSwingsInDescription: {
+    id: 'axsmap.components.CreateReview.doorSwingsInDescription',
+    defaultMessage:
+      'Current guidelines for bathrooms allow the bathroom door to swing inward “as long as there is a clear floor space” beyond the door swing, when open.'
+  },
+  doorSwingsOutTitle: {
+    id: 'axsmap.components.CreateReview.doorSwingsOutTitle',
+    defaultMessage: 'Door swings out'
+  },
+  doorSwingsOutDescription: {
+    id: 'axsmap.components.CreateReview.doorSwingsOutDescription',
+    defaultMessage:
+      'Current guidelines for bathrooms allow the bathroom door to swing inward “as long as there is a clear floor space” beyond the door swing, when open.'
+  },
+  largeStallsTitle: {
+    id: 'axsmap.components.CreateReview.largeStallsTitle',
+    defaultMessage: 'large stalls'
+  },
+  largeStallsDescription: {
+    id: 'axsmap.components.CreateReview.largeStallsDescription',
+    defaultMessage:
+      'The minimum floorspace should be 30 inches by 48 inches, with plenty of room for a wheelchair next to the toilet.'
+  },
+  tallSinksTitle: {
+    id: 'axsmap.components.CreateReview.tallSinksTitle',
+    defaultMessage: 'Tall sinks'
+  },
+  tallSinksDescription: {
+    id: 'axsmap.components.CreateReview.tallSinksDescription',
+    defaultMessage: 'Sinks taller than 34 inches.'
+  },
+  loweredSinksTitle: {
+    id: 'axsmap.components.CreateReview.loweredSinksTitle',
+    defaultMessage: 'Lowered sinks'
+  },
+  loweredSinksDescription: {
+    id: 'axsmap.components.CreateReview.loweredSinksDescription',
+    defaultMessage: 'Sinks mounted no more than 34 inches from the floor.'
+  },
+  noSupportAroundToiletTitle: {
+    id: 'axsmap.components.CreateReview.noSupportAroundToiletTitle',
+    defaultMessage: 'No support around toilet'
+  },
+  noSupportAroundToiletDescription: {
+    id: 'axsmap.components.CreateReview.noSupportAroundToiletDescription',
+    defaultMessage: 'No metal handrail.'
+  },
+  oneBarAroundToiletTitle: {
+    id: 'axsmap.components.CreateReview.oneBarAroundToiletTitle',
+    defaultMessage: 'one bar support around toilet'
+  },
+  oneBarAroundToiletDescription: {
+    id: 'axsmap.components.CreateReview.oneBarAroundToiletDescription',
+    defaultMessage: 'Metal handrail mounted on the wall beside the toilet.'
+  },
+  twoBarAroundToiletTitle: {
+    id: 'axsmap.components.CreateReview.twoBarAroundToiletTitle',
+    defaultMessage: 'two bar support around toilet'
+  },
+  twoBarAroundToiletDescription: {
+    id: 'axsmap.components.CreateReview.twoBarAroundToiletDescription',
+    defaultMessage: 'Metal handrail mounted on the wall behind the toilet.'
+  },
   bathroomTitle: {
     id: 'axsmap.components.CreateReview.bathroomTitle',
     defaultMessage: 'Bathroom'
@@ -44,10 +237,6 @@ export default defineMessages({
   stepsTitle: {
     id: 'axsmap.components.CreateReview.stepsTitle',
     defaultMessage: 'Interior'
-  },
-  guideDogTitle: {
-    id: 'axsmap.components.CreateReview.guideDogTitle',
-    defaultMessage: 'Guide dog'
   },
   parkingTitle: {
     id: 'axsmap.components.CreateReview.parkingTitle',
@@ -57,17 +246,9 @@ export default defineMessages({
     id: 'axsmap.components.CreateReview.secondEntryTitle',
     defaultMessage: 'Second entry'
   },
-  wellLitTitle: {
-    id: 'axsmap.components.CreateReview.wellLitTitle',
-    defaultMessage: 'Well lit'
-  },
-  quietTitle: {
-    id: 'axsmap.components.CreateReview.quietTitle',
-    defaultMessage: 'Quiet'
-  },
-  spaciousTitle: {
-    id: 'axsmap.components.CreateReview.spaciousTitle',
-    defaultMessage: 'Spacious'
+  exitButton: {
+    id: 'axsmap.components.CreateReview.exitButton',
+    defaultMessage: 'Exit Review'
   },
   yesButton: {
     id: 'axsmap.components.CreateReview.yesButton',
