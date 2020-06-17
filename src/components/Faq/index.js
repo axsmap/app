02557beeba9f232832b8e844d2c93102ac339b72
ivan-f-aguirre <ivan.f.aguirre@pwc.com>
2020-06-17 14:01:39 -0400

    Leigh changes for new functionality

--- src/components/Faq/index.js
+++ src/components/Faq/index.js
@@ -14,9 +14,7 @@ import Wrp from '../Wrapper'
 
 import messages from './messages'
 
-const Wrapper = styled(Wrp)`
-  padding-bottom: 0;
-`
+const Wrapper = styled(Wrp)`padding-bottom: 0;`
 
 const Title = styled.h1`
   display: none;
@@ -56,13 +54,13 @@ class Faq extends React.Component {
   }
 
   render() {
-    const {formatMessage} = this.context.intl
+    const formatMessage = this.context.intl.formatMessage
 
     return (
       <Wrapper>
         <Helmet title={formatMessage(messages.pageTitle)} />
 
-        <TopBar hideOn="phone,tablet"  />
+        <TopBar hideOn="phone,tablet" />
 
         <NavBar
           hideOn="desktop,widescreen"
@@ -71,22 +69,12 @@ class Faq extends React.Component {
           goBackHandler={() => this.props.history.goBack()}
         />
 
-        <Container  className="mx-auto" >
+        <Container>
           <Title>{formatMessage(messages.headerTitle)}</Title>
 
           <Video
             title="video-1"
-            src="https://www.youtube.com/embed/4Chta29jjXQ?rel=0"
-            frameBorder="0"
-            allow="autoplay; encrypted-media"
-            allowFullscreen
-          />
-
-          <h2>How do I rate a location?</h2>
-
-          <Video
-            title="video-1"
-            src="https://www.youtube.com/embed/DFswxhQj6UI?rel=0"
+            src="https://www.youtube.com/embed/7iUUeLaiUBE?rel=0"
             frameBorder="0"
             allow="autoplay; encrypted-media"
             allowFullscreen
@@ -94,98 +82,40 @@ class Faq extends React.Component {
 
           <br />
 
-          <p>
-            This video explains it best. It&#39;s short and provides important
-            background especially for able-bodied people who may not have
-            experience being around people using wheelchairs, power chairs, or
-            scooters. We here at AXS Map encourage everyone to contribute
-            ratings on accessibility!
-          </p>
 
-          <h2>How do I rate the entryway?</h2>
+          <h2>How do I rate a venue?</h2>
           <p>
             <span>
-              Consider the following criteria when rating an entryway:
+              AXS Map takes you through an intuitive rating process, but consider the following criteria when rating a venue:
             </span>
           </p>
           <ul>
             <li>
-              <span>1 star</span>
+              <span>Entrance</span>
               <span>
-                &nbsp;&ndash; narrow, multiple steps to climb, no ramp available
+                &nbsp;&ndash; does the entrance have steps, a ramp, reserved parking, a second entrance, or a wide entrance?
               </span>
             </li>
             <li>
-              <span>2 stars</span>
+              <span>Interior</span>
               <span>
-                &nbsp;&ndash; narrow, one step to climb, no ramp available
+                &nbsp;&ndash; does the interior have room to move, a ramp, an accessible elevator, or an accessible table height? 
               </span>
             </li>
             <li>
-              <span>3 stars</span>
+              <span>Restroom</span>
               <span>
-                &nbsp;&ndash; portable ramp available, too steep or difficult to
-                navigate
+                &nbsp;&ndash; does the restroom door swing out, have large stalls, supports around the toilet, or lowered sinks?
               </span>
             </li>
             <li>
-              <span>4 stars</span>
+              <span>Other</span>
               <span>
-                &nbsp;&ndash; wide entrance with steps, portable ramp available
-                that is accessible
-              </span>
-            </li>
-            <li>
-              <span>5 stars</span>
-              <span>
-                &nbsp;&ndash; wide entrance, no steps or has permanent ramp,
-                easily accessible
+                &nbsp;&ndash; is the venue well-lit, have a high noise level, or allow guide dogs?
               </span>
             </li>
           </ul>
-          <h2>
-            <span>How do I rate the bathroom?</span>
-          </h2>
-          <p>
-            <span>Consider the following criteria when rating a bathroom:</span>
-          </p>
-          <ul>
-            <li>
-              <span>1 star</span>
-              <span>
-                &nbsp;&ndash; door swings in, small stalls, tall sinks, no bar
-                supports around toilet
-              </span>
-            </li>
-            <li>
-              <span>2 stars</span>
-              <span>
-                &nbsp;&ndash; door swings in, average stalls, tall sinks, no bar
-                supports around toilet
-              </span>
-            </li>
-            <li>
-              <span>3 stars</span>
-              <span>
-                &nbsp;&ndash; door swings out, large stalls, tall sinks, no bar
-                supports around toilet
-              </span>
-            </li>
-            <li>
-              <span>4 stars</span>
-              <span>
-                &nbsp;&ndash; door swings out, large stalls, lowered sinks, one
-                bar support around toilet
-              </span>
-            </li>
-            <li>
-              <span>5 stars</span>
-              <span>
-                &nbsp;&ndash; door swings out, large stalls, lowered sinks, two
-                bar supports around toilet
-              </span>
-            </li>
-          </ul>
+          
           <h2>
             <span>Can I add a place that&#39;s not on AXS Map?</span>
           </h2>
@@ -194,8 +124,7 @@ class Faq extends React.Component {
               No&mdash;AXS Map pulls from the Google Places database to populate
               its maps. Google Places is working to catalogue businesses around
               the world. If a place is missing, we recommend submitting an
-              inquiry to
-              {' '}
+              inquiry to{' '}
             </span>
             <span>
               <a href="https://www.google.com/url?q=https://www.google.com/local/business/add?migratedId%3D06188353489892148674&amp;sa=D&amp;ust=1520224188105000&amp;usg=AFQjCNGl63hbmMkVSmDeWeK5pUKAMYzLyA">
@@ -215,8 +144,7 @@ class Faq extends React.Component {
               We need communities to band together to map their neighborhoods.
               At AXS Map we&rsquo;ve started a special tool to help you and your
               community do this. It&#39;s called the &#39;Mapathon&#39; tool,
-              &amp;
-              {' '}
+              &amp;{' '}
             </span>
             <span>
               <a href="/mapathons">you should check it out!</a>
@@ -227,8 +155,7 @@ class Faq extends React.Component {
               group of people together to go out and strategically map out the
               neighborhood. If you don&#39;t want to start a Mapathon, you can
               join one&mdash;we offer all the tools for you to make it happen.
-              Check out the
-              {' '}
+              Check out the{' '}
             </span>
             <span>
               <a href="/mapathons">Mapathons page here</a>
@@ -242,8 +169,7 @@ class Faq extends React.Component {
             <span>
               Absolutely. If you have been to a place and have a good memory of
               it (perhaps it&#39;s a place you frequent) feel free to give a
-              review!
-              {' '}
+              review!{' '}
             </span>
           </p>
           <h2>
@@ -254,16 +180,14 @@ class Faq extends React.Component {
               No, you do not need to join in order to use AXS Map. Without
               joining, you will still be able to see all the reviews on AXS Map,
               and you will have full access to everything on our maps. But if
-              you would like to give reviews we will need you to join first.
-              {' '}
+              you would like to give reviews we will need you to join first.{' '}
             </span>
             <span>
               <a href="/sign-up">Joining is free and easy</a>
             </span>
             <span>, &amp; a</span>
             <span>
-              ll information you provide to AXS Map will remain anonymous.
-              {' '}
+              ll information you provide to AXS Map will remain anonymous.{' '}
             </span>
           </p>
           <h2>
@@ -283,8 +207,7 @@ class Faq extends React.Component {
               search bar on the right. If you are in a dense city like New York
               City it is helpful to type in the name of the location on the left
               and either a nearby address or the actual address in the right
-              hand box.
-              {' '}
+              hand box.{' '}
             </span>
           </p>
           <p>
@@ -299,8 +222,7 @@ class Faq extends React.Component {
               hit the &lsquo;Refresh&rsquo; button (magnifying glass icon) in
               the lower right corner of the map to refresh the results. Each
               time you hit the Refresh button, the map will return 20 new pins
-              in the area you&rsquo;ve zoomed in on.
-              {' '}
+              in the area you&rsquo;ve zoomed in on.{' '}
             </span>
           </p>
           <p>
@@ -337,63 +259,53 @@ class Faq extends React.Component {
           </h2>
 
           <p>
-            <b>Paper review forms:</b>
-            {' '}
+            <b>Paper review forms:</b>{' '}
             <a
               href="https://s3.amazonaws.com/axsmap-media/documents/AXS+Map+Paper+Entry+Form+-+For+people+without+phones.xlsx"
               target="_blank"
               rel="noopener noreferrer"
             >
               These
-            </a>
-            {' '}
+            </a>{' '}
             forms allow your participants to enter reviews on paper. This is a
             good thing to have in case someone’s phone doesn’t work or in case a
             participant doesn’t have a phone.
           </p>
 
           <p>
-            <b>Brochure for handing out to businesses:</b>
-            {' '}
-When your Mapathon
+            <b>Brochure for handing out to businesses:</b> When your Mapathon
             participants are out roaming the neighborhoods, sometimes it can be
             a bit startling to a business owner when they request to ‘see’ their
-            bathroom.
-            {' '}
+            bathroom.{' '}
             <a
               href="https://s3.amazonaws.com/axsmap-media/documents/AXS+Map+-+For+the+Business+Owner+or+Manager.docx"
               target="_blank"
               rel="noopener noreferrer"
             >
               Here’s
-            </a>
-            {' '}
+            </a>{' '}
             a brochure to help your Mapathoners introduce themselves to a
             business owner politely and explain what they’re doing.
           </p>
 
           <p>
-            <b>How to rate:</b>
-            {' '}
+            <b>How to rate:</b>{' '}
             <a
               href="https://s3.amazonaws.com/axsmap-media/documents/AXS+Map-+How+to+Rate+Guidance.docx"
               target="_blank"
               rel="noopener noreferrer"
             >
               Guidance on Giving Reviews
-            </a>
-            {' '}
+            </a>{' '}
             - Some of your participants may like to see some guidance on giving
-            reviews.
-            {' '}
+            reviews.{' '}
             <a
               href="https://s3.amazonaws.com/axsmap-media/documents/AXS+Map+-+Tips+and+Tricks+of+the+Trade.docx"
               target="_blank"
               rel="noopener noreferrer"
             >
               This
-            </a>
-            {' '}
+            </a>{' '}
             document will provide some tips and instructions.
           </p>
 
@@ -465,8 +377,7 @@ When your Mapathon
               <span>
                 &nbsp; If you want your teams to compete, make sure volunteers
                 join teams using the AXS Map tool. This way we&rsquo;ll be able
-                to track each team&rsquo;s number of reviews.
-                {' '}
+                to track each team&rsquo;s number of reviews.{' '}
               </span>
             </li>
             <li>
@@ -474,8 +385,7 @@ When your Mapathon
               <span>
                 : &nbsp;It is important that teams know what blocks of a city to
                 cover. This can take the form of assigning a zip code, or
-                setting up a 5 by 5 block area for each team.
-                {' '}
+                setting up a 5 by 5 block area for each team.{' '}
               </span>
             </li>
             <li>
