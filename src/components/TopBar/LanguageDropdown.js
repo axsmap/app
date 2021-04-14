bb03733ad494782fb21ad23bc7b344e979262694
haseeb khan <haseebadilkhan@gmail.com>
2021-04-14 16:56:56 -0600

    adding language dropdown to topbar

---
+++ src/components/TopBar/LanguageDropdown.js
@@ -0,0 +1,173 @@
+import { intlShape } from "react-intl";
+import PropTypes from "prop-types";
+import React, { PureComponent } from "react";
+import { rgba } from "polished";
+import styled from "styled-components";
+
+import RouterLink from "../RouterLink";
+import { colors, media, fontSize } from "../../styles";
+import worldImage from "../../images/icons/world.png";
+
+import messages from "./messages";
+
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: inherit;
+`;
+
+const Link = styled(RouterLink)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: inherit;
+  margin-right: 0.2rem;
+  padding: 0 0.5rem;
+  width: 100%;
+  color: ${colors.darkestGrey};
+  font-weight: bold;
+  font-size: ${fontSize.sm};
+  text-decoration: none;
+  text-transform: uppercase;
+
+  &:active,
+  &:focus {
+    box-shadow: inset 0px 0px 0px 2px ${colors.secondary};
+    outline: none;
+  }
+
+  &:hover {
+    color: ${colors.secondary};
+  }
+
+  ${media.tablet`
+    font-size: ${fontSize.xs};
+  `};
+
+  @media (min-width: 1200px) and (max-width: 1299px) {
+    font-size: ${fontSize.xxxs}!important;
+  }
+
+  ${media.desktop`
+    font-size: ${fontSize.xs};
+  `};
+
+  ${media.widescreen`
+    font-size: ${fontSize.sm};
+  `};
+`;
+
+const Image = styled.img`
+  border-radius: 100%;
+  height: 2rem;
+  width: inherit;
+`;
+
+const Bar = styled.div`
+  bottom: 0;
+  left: 0;
+  position: absolute;
+
+  display: ${(props) => (props.isVisible ? "block" : "none")};
+
+  height: 2px;
+  width: 100%;
+
+  background-color: ${colors.primary};
+`;
+
+const Dropdown = styled.div`
+  position: absolute;
+  right: 0;
+  top: 100%;
+
+  display: ${(props) => (props.isVisible ? "flex" : "none")};
+
+  flex-direction: column;
+
+  box-shadow: 0 1px 0 0 ${colors.grey};
+  padding: 0.5rem;
+  width: 100%;
+
+  background-color: white;
+
+  ${media.desktop`
+    display: ${(props) => (props.isVisible ? "flex" : "none")};
+  `};
+`;
+
+const Icon = styled.img`
+  height: 1.5rem;
+  padding-right: 0.5rem;
+`;
+
+const Button = styled.button`
+  appearance: none;
+  border: none;
+  border-radius: 3px;
+  box-shadow: none;
+  height: 3rem;
+  margin: 0;
+  padding: 0.5em;
+  width: 100%;
+
+  background-color: ${colors.alert};
+  cursor: pointer;
+
+  color: white;
+  font-size: 1rem;
+  font-weight: bold;
+  text-transform: uppercase;
+
+  &:active,
+  &:focus {
+    outline: 2px solid ${colors.secondary};
+  }
+
+  &:disabled,
+  &[disabled] {
+    background-color: ${rgba(colors.alert, 0.5)};
+    color: ${rgba("white", 0.5)};
+  }
+`;
+
+class LanguageDropdown extends PureComponent {
+  static propTypes = {};
+
+  static contextTypes = {
+    intl: intlShape,
+  };
+
+  state = {
+    showDropdown: true,
+  };
+
+  hideDropdown = () => this.setState({ showDropdown: false });
+
+  showDropdown = () => this.setState({ showDropdown: true });
+
+  render() {
+    return (
+      <Wrapper
+        onMouseEnter={this.showDropdown}
+        onMouseLeave={this.hideDropdown}
+      >
+        <Link to="/" aria-label="Select your preferred language">
+          <Icon src={worldImage} alt="Language Select" />
+          English
+        </Link>
+
+        <Bar isVisible={this.props.isActive} />
+
+        <Dropdown isVisible="{this.state.showDropdown}">
+          <Button />
+        </Dropdown>
+      </Wrapper>
+    );
+  }
+}
+
+export default LanguageDropdown;
