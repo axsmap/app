678f79ff8f926fc72bc4576688796a21cb43ca88
haseeb khan <haseebadilkhan@gmail.com>
2021-05-16 18:52:06 -0600

    connecting redux for lang dropddown

--- src/components/TopBar/Language.js
+++ src/components/TopBar/Language.js
@@ -1,10 +1,11 @@
-import { intlShape } from "react-intl";
-import PropTypes, { func } from "prop-types";
-import React, { Component } from "react";
+import { intlShape } from 'react-intl'
+import PropTypes, { func } from 'prop-types'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-import styled from "styled-components";
-import LanguageProvider from "../../containers/LanguageProvider/index";
-
+import styled from 'styled-components'
+import LanguageProvider from '../../containers/LanguageProvider/index'
+import changeLocale from '../../containers/LanguageProvider/actions'
 const LanguageBtn = styled.a`
   color: black;
   padding: 12px 16px;
@@ -14,41 +15,27 @@ const LanguageBtn = styled.a`
   &:hover {
     background-color: #f1f1f1;
   }
-`;
+`
 
 class LanguageButton extends Component {
   static propTypes = {
     onClickHandler: func.isRequired,
     locale: PropTypes.string.isRequired,
-    language: PropTypes.string.isRequired,
-  };
+    language: PropTypes.string.isRequired
+  }
 
   state = {
-    locale: this.props.locale,
-  };
+    locale: this.props.locale
+  }
 
-  onClickHandler = (action) => {
-    if (!action) return;
-    if (this.props.language === "English") {
-      console.log(this.props.language);
-      this.setState({ locale: "en" });
-      console.log(this.state);
-      LanguageProvider.props = {
-        locale: this.props.locale,
-        key: this.props.locale,
-      };
-      console.log(LanguageProvider.props);
-    } else if (this.props.language === "Spanish") {
-      console.log(this.props.language);
-      this.setState({ locale: "es" });
-      console.log(this.state);
-      LanguageProvider.props = {
-        locale: this.props.locale,
-        key: this.props.locale,
-      };
-      console.log(LanguageProvider.props);
+  onClickHandler = action => {
+    if (!action) return
+    if (this.props.language === 'English') {
+      this.props.changeLocale('en')
+    } else if (this.props.language === 'Spanish') {
+      this.props.changeLocale('es')
     }
-  };
+  }
 
   render = () => {
     return (
@@ -59,8 +46,19 @@ class LanguageButton extends Component {
       >
         {this.props.language}
       </LanguageBtn>
-    );
-  };
+    )
+  }
 }
 
-export default LanguageButton;
+const mapDispatchToProps = dispatch => ({
+  changeLocale: language => {
+    dispatch(changeLocale(language))
+  }
+})
+
+LanguageButton = connect(
+  null,
+  mapDispatchToProps
+)(LanguageButton)
+
+export default LanguageButton
