aa0eee961704036a88aed5a9a2d89e12e8b8bfea
haseeb khan <haseebadilkhan@gmail.com>
2021-05-17 18:56:28 -0600

    making LANGUAGE text in topbar responsive

--- src/components/TopBar/LanguageDropdown.js
+++ src/components/TopBar/LanguageDropdown.js
@@ -1,23 +1,23 @@
-import React, { Component } from "react";
-import { rgba } from "polished";
-import styled from "styled-components";
-import PropTypes from "prop-types";
+import React, { Component } from 'react'
+import { rgba } from 'polished'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
-import { colors, media, fontSize } from "../../styles";
-import worldImage from "../../images/icons/world.png";
+import { colors, media, fontSize } from '../../styles'
+import worldImage from '../../images/icons/world.png'
 
-import Language from "./Language";
+import Language from './Language'
 
 const StyledUl = styled.ul`
   list-style-type: none;
   margin: 0;
   padding: 0;
   overflow: hidden;
-`;
+`
 
 const StyledLi = styled.li`
   float: left;
-`;
+`
 
 const Dropbtn = styled.div`
   display: flex;
@@ -28,7 +28,9 @@ const Dropbtn = styled.div`
   padding: 0 0.5rem;
   padding-right: 1.5rem;
   width: 100%;
-  color: ${colors.darkestGrey}; // dark mode and light mode language text color - blue
+  color: ${
+    colors.darkestGrey
+  }; // dark mode and light mode language text color - blue
   font-size: 0.85rem;
   font-weight: bold;
   text-decoration: none;
@@ -36,9 +38,27 @@ const Dropbtn = styled.div`
   cursor: pointer;
 
   &:hover {
-    color: ${colors.secondary}; // dark mode and light mode language text hover color
+    color: ${
+      colors.secondary
+    }; // dark mode and light mode language text hover color
   }
-`;
+    ${media.tablet`
+    font-size: ${fontSize.xs};
+  `};
+
+  @media (min-width:1200px) and (max-width:1299px){
+    font-size: ${fontSize.xxxs} !important
+    padding: 8px;
+  }
+
+  ${media.desktop`
+    font-size: ${fontSize.xs};
+  `};
+
+  ${media.widescreen`
+    font-size: ${fontSize.sm};
+  `};
+`
 
 const DropDownContent = styled.div`
   display: none;
@@ -47,7 +67,7 @@ const DropDownContent = styled.div`
   min-width: 130px;
   box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
   z-index: 1;
-`;
+`
 
 const DropDownLi = styled(StyledLi)`
   display: inline-block;
@@ -55,40 +75,40 @@ const DropDownLi = styled(StyledLi)`
   &:hover ${DropDownContent} {
     display: block;
   }
-`;
+`
 
 const Icon = styled.img`
   height: 1.25rem;
   padding-right: 0.5rem;
-`;
+`
 
 class LanguageDropdown extends Component {
   render = () => {
-    LanguageDropdown.propTypes = { label: PropTypes.string.isRequired };
+    LanguageDropdown.propTypes = { label: PropTypes.string.isRequired }
     return (
       <StyledUl>
         <DropDownLi>
           <Dropbtn>
             <Icon srcSet={worldImage} alt="language selector" />
-            {this.props.label}{" "}
+            {this.props.label}{' '}
           </Dropbtn>
           <DropDownContent className="language">
-            {" "}
+            {' '}
             <Language
               language="English"
               locale="en"
-              onClick={() => this.onClickHandler("en")}
+              onClick={() => this.onClickHandler('en')}
             />
             <Language
               language="Spanish"
               locale="es"
-              onClick={() => this.onClickHandler("es")}
+              onClick={() => this.onClickHandler('es')}
             />
           </DropDownContent>
         </DropDownLi>
       </StyledUl>
-    );
-  };
+    )
+  }
 }
 
-export default LanguageDropdown;
+export default LanguageDropdown
