35a766dd9b973aad37316d9a2c527aa7fb21dd3f
Haseeb Adil Khan <haseebadilkhan@gmail.com>
2022-01-03 17:15:05 -0700

    changing Create a Mapathon header to Create for english lang

--- src/components/CreateMapathon/messages.js
+++ src/components/CreateMapathon/messages.js
@@ -3,176 +3,176 @@ import { defineMessages } from 'react-intl'
 export default defineMessages({
   pageTitle: {
     id: 'axsmap.components.CreateMapathon.pageTitle',
-    defaultMessage: 'Create a Mapathon | AXS Map'
+    defaultMessage: 'Create a Mapathon | AXS Map',
   },
   headerTitle: {
     id: 'axsmap.components.CreateMapathon.headerTitle',
-    defaultMessage: 'Create a Mapathon'
+    defaultMessage: 'Create',
   },
   nameLabel: {
     id: 'axsmap.components.CreateMapathon.nameLabel',
-    defaultMessage: 'Name'
+    defaultMessage: 'Name',
   },
   namePlaceholder: {
     id: 'axsmap.components.CreateMapathon.namePlaceholder',
-    defaultMessage: 'Name your Mapathon (eg. New York City Mapathon)'
+    defaultMessage: 'Name your Mapathon (eg. New York City Mapathon)',
   },
   nameError1: {
     id: 'axsmap.components.CreateMapathon.nameError1',
-    defaultMessage: 'Is required'
+    defaultMessage: 'Is required',
   },
   nameError2: {
     id: 'axsmap.components.CreateMapathon.nameError2',
-    defaultMessage: 'Should be less than 101 characters'
+    defaultMessage: 'Should be less than 101 characters',
   },
   nameError3: {
     id: 'axsmap.components.CreateMapathon.nameError3',
-    defaultMessage: 'Is already taken'
+    defaultMessage: 'Is already taken',
   },
   descriptionLabel: {
     id: 'axsmap.components.CreateMapathon.descriptionLabel',
-    defaultMessage: 'Mapathon description'
+    defaultMessage: 'Mapathon description',
   },
   descriptionPlaceholder: {
     id: 'axsmap.components.CreateMapathon.descriptionPlaceholder',
     defaultMessage:
-      'Write a short blurb about your Mapathon (eg. Team NYC wants you to come out this Sunday to help add accessibility data to our neighborhood!)'
+      'Write a short blurb about your Mapathon (eg. Team NYC wants you to come out this Sunday to help add accessibility data to our neighborhood!)',
   },
   descriptionError: {
     id: 'axsmap.components.CreateMapathon.descriptionError',
-    defaultMessage: 'Should be less than 301 characters'
+    defaultMessage: 'Should be less than 301 characters',
   },
   addressLabel: {
     id: 'axsmap.components.CreateMapathon.addressLabel',
-    defaultMessage: 'Starting point'
+    defaultMessage: 'Starting point',
   },
   addressPlaceholder: {
     id: 'axsmap.components.CreateMapathon.addressPlaceholder',
     defaultMessage:
-      'Enter your starting point address (eg. 405 East 42nd Street, New York, NY) or move the pin in the map below'
+      'Enter your starting point address (eg. 405 East 42nd Street, New York, NY) or move the pin in the map below',
   },
   addressError1: {
     id: 'axsmap.components.CreateMapathon.addressError1',
-    defaultMessage: 'Is required'
+    defaultMessage: 'Is required',
   },
   addressError2: {
     id: 'axsmap.components.CreateMapathon.addressError2',
-    defaultMessage: 'Should be less than 201 characters'
+    defaultMessage: 'Should be less than 201 characters',
   },
   locationLabel: {
     id: 'axsmap.components.CreateMapathon.locationLabel',
-    defaultMessage: 'Location'
+    defaultMessage: 'Location',
   },
   datesLabel: {
     id: 'axsmap.components.CreateMapathon.datesLabel',
-    defaultMessage: 'Start date and end date'
+    defaultMessage: 'Start date and end date',
   },
   datesError: {
     id: 'axsmap.components.CreateMapathon.datesError',
-    defaultMessage: 'Dates are required'
+    defaultMessage: 'Dates are required',
   },
   startDateError: {
     id: 'axsmap.components.CreateMapathon.startDateError',
-    defaultMessage: 'Start date is required'
+    defaultMessage: 'Start date is required',
   },
   endDateError: {
     id: 'axsmap.components.CreateMapathon.endDateError',
-    defaultMessage: 'End date is required'
+    defaultMessage: 'End date is required',
   },
   isOpenLabel: {
     id: 'axsmap.components.CreateMapathon.isOpenLabel',
-    defaultMessage: 'Make this Mapathon open to the public'
+    defaultMessage: 'Make this Mapathon open to the public',
   },
   participantsGoalLabel: {
     id: 'axsmap.components.CreateMapathon.participantsGoalLabel',
-    defaultMessage: 'How many participants will be joining?'
+    defaultMessage: 'How many participants will be joining?',
   },
   participantsGoalError1: {
     id: 'axsmap.components.CreateMapathon.participantsGoalError1',
-    defaultMessage: 'Is required'
+    defaultMessage: 'Is required',
   },
   participantsGoalError2: {
     id: 'axsmap.components.CreateMapathon.participantsGoalError2',
-    defaultMessage: 'Should be greater than 0'
+    defaultMessage: 'Should be greater than 0',
   },
   participantsGoalError3: {
     id: 'axsmap.components.CreateMapathon.participantsGoalError3',
-    defaultMessage: 'Should be less than 1001'
+    defaultMessage: 'Should be less than 1001',
   },
   addPosterButton: {
     id: 'axsmap.components.CreateMapathon.addPosterButton',
-    defaultMessage: 'Add poster'
+    defaultMessage: 'Add poster',
   },
   reviewsGoalLabel: {
     id: 'axsmap.components.CreateMapathon.reviewsGoalLabel',
-    defaultMessage: "What's your review goal?"
+    defaultMessage: "What's your review goal?",
   },
   reviewsGoalError1: {
     id: 'axsmap.components.CreateMapathon.reviewsGoalError1',
-    defaultMessage: 'Is required'
+    defaultMessage: 'Is required',
   },
   reviewsGoalError2: {
     id: 'axsmap.components.CreateMapathon.reviewsGoalError2',
-    defaultMessage: 'Should be greater than 0'
+    defaultMessage: 'Should be greater than 0',
   },
   reviewsGoalError3: {
     id: 'axsmap.components.CreateMapathon.reviewsGoalError3',
-    defaultMessage: 'Should be less than 10001'
+    defaultMessage: 'Should be less than 10001',
   },
   hostAsLabel: {
     id: 'axsmap.components.CreateMapathon.hostAsLabel',
-    defaultMessage: 'Host as'
+    defaultMessage: 'Host as',
   },
   individualLabel: {
     id: 'axsmap.components.CreateMapathon.individualLabel',
-    defaultMessage: 'An individual'
+    defaultMessage: 'An individual',
   },
   teamLabel: {
     id: 'axsmap.components.CreateMapathon.teamLabel',
-    defaultMessage: 'A team'
+    defaultMessage: 'A team',
   },
   chooseManagerButton: {
     id: 'axsmap.components.CreateMapathon.chooseManagerButton',
-    defaultMessage: 'Choose'
+    defaultMessage: 'Choose',
   },
   chooseManagerPlaceholder: {
     id: 'axsmap.components.CreateMapathon.chooseManagerPlaceholder',
-    defaultMessage: 'Search your teams'
+    defaultMessage: 'Search your teams',
   },
   noTeamsResultsText: {
     id: 'axsmap.components.CreateMapathon.noTeamsResultsText',
-    defaultMessage: 'No teams found'
+    defaultMessage: 'No teams found',
   },
   donationLabel: {
     id: 'axsmap.components.CreateMapathon.donationLabel',
-    defaultMessage: 'Yes! I want to make this a Fundraising Event for AXS Lab'
+    defaultMessage: 'Yes! I want to make this a Fundraising Event for AXS Lab',
   },
   donationAmountsLabel: {
     id: 'axsmap.components.CreateMapathon.donationAmountsLabel',
-    defaultMessage: 'Donation amounts (3 max)'
+    defaultMessage: 'Donation amounts (3 max)',
   },
   donationAmountDescriptionPlaceholder: {
     id: 'axsmap.components.CreateMapathon.donationAmountDescriptionPlaceholder',
-    defaultMessage: 'What provides this amount?'
+    defaultMessage: 'What provides this amount?',
   },
   donationGoalLabel: {
     id: 'axsmap.components.CreateMapathon.donationGoalLabel',
-    defaultMessage: 'Donation goal'
+    defaultMessage: 'Donation goal',
   },
   donationGoalError1: {
     id: 'axsmap.components.CreateMapathon.donationGoalError1',
-    defaultMessage: 'Is required'
+    defaultMessage: 'Is required',
   },
   donationGoalError2: {
     id: 'axsmap.components.CreateMapathon.donationGoalError2',
-    defaultMessage: 'Should be greater than 9'
+    defaultMessage: 'Should be greater than 9',
   },
   donationGoalError3: {
     id: 'axsmap.components.CreateMapathon.donationGoalError3',
-    defaultMessage: 'Should be less than 100001'
+    defaultMessage: 'Should be less than 100001',
   },
   createMapathonButton: {
     id: 'axsmap.components.CreateTeam.createMapathonButton',
-    defaultMessage: 'Create mapathon'
-  }
+    defaultMessage: 'Create mapathon',
+  },
 })
