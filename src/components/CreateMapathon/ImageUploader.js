a6739b7a0ee330df1e90998d6cdf2da2461a6844
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2022-03-06 13:32:51 -0700

    modal created + partial styling

--- src/components/CreateMapathon/ImageUploader.js
+++ src/components/CreateMapathon/ImageUploader.js
@@ -1,8 +1,10 @@
-import { AddCircle } from '@material-ui/icons'
+import { Modal, Box, Typography, Grid } from '@material-ui/core'
+import { AddCircle, Close } from '@material-ui/icons'
 import React, { useEffect, useRef, useState } from 'react'
 
 import styled from 'styled-components'
 import { colors, fonts, media, fontWeight, fontSize } from '../../styles'
+
 
 const ImageUploadDiv = styled.div`
   display: flex;
@@ -35,10 +37,46 @@ const InnerUploadDiv = styled.div`
     padding: 2rem 0;
   `};
 `
+
+const ModalContainerDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  width: 100%;
+  height: 100%
+`
+
+const InnerModalDiv = styled.div`
+  height: 70%;
+  width: 40%;
+  border: 4px solid green;
+  background-color: white;
+
+
+  ${media.desktop`
+    padding: 2rem 0;
+  `};
+`
+
+const ModalGrid = styled.div`
+  display: grid
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: 50px 50px
+  grid-gap: 5px
+`
+
+const ModalItem = styled.div`
+  display: flex
+  justify-content: center
+  padding: .5rem
+`
+
 export default function ImageUploader() {
   const [images, setImages] = useState([])
   const [imageURLs, setImageURLs] = useState([])
   const [hide, setHide] = useState(true)
+  const [open, setOpen] = useState(false)
   const inputRef = useRef(null)
 
   useEffect(
@@ -51,6 +89,9 @@ export default function ImageUploader() {
     [images]
   )
 
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+  
   function onImageChange(e) {
     setHide(false)
     setImages([...e.target.files])
@@ -69,7 +110,7 @@ export default function ImageUploader() {
         Upload image
       </button> */}
       {hide ? (
-        <ImageUploadDiv onClick={() => inputRef.current.click()}>
+        <ImageUploadDiv onClick={handleOpen}>
           <InnerUploadDiv>
             <AddCircle fontSize="large" />
             Add a photo
@@ -77,6 +118,26 @@ export default function ImageUploader() {
         </ImageUploadDiv>
       ) : null}
 
+      <Modal
+        open={open}
+        onClose={handleClose}
+      >
+        <ModalContainerDiv>
+          <InnerModalDiv>
+            <Close fontSize='large' style={{marginTop: '-1rem'}}/>
+            <Typography style={{paddingLeft: '1rem', marginTop: '1rem'}}>Add or select an image that represents your story</Typography>
+            <ModalGrid>
+              <ModalItem>1</ModalItem>
+              <ModalItem>2</ModalItem>
+              <ModalItem>3</ModalItem>
+              <ModalItem>4</ModalItem>
+              <ModalItem>5</ModalItem>
+              <ModalItem>6</ModalItem>
+            </ModalGrid>
+          </InnerModalDiv>
+        </ModalContainerDiv>
+
+      </Modal>
       {imageURLs.map(imageSrc => <img src={imageSrc} alt="testImage" />)}
     </>
   )
