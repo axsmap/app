99b576f2b8bdd808d3c49775d6aa09b33e4b0d62
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2022-03-06 14:25:08 -0700

    more styling fixes and added upload funcionality back

--- src/components/CreateMapathon/ImageUploader.js
+++ src/components/CreateMapathon/ImageUploader.js
@@ -1,10 +1,9 @@
 import { Modal, Box, Typography, Grid } from '@material-ui/core'
-import { AddCircle, Close } from '@material-ui/icons'
+import { AddCircle, Close, CloudUpload } from '@material-ui/icons'
 import React, { useEffect, useRef, useState } from 'react'
-
+import { FaUpload } from 'react-icons/fa'
 import styled from 'styled-components'
 import { colors, fonts, media, fontWeight, fontSize } from '../../styles'
-
 
 const ImageUploadDiv = styled.div`
   display: flex;
@@ -44,7 +43,7 @@ const ModalContainerDiv = styled.div`
   align-items: center;
   margin: auto;
   width: 100%;
-  height: 100%
+  height: 100%;
 `
 
 const InnerModalDiv = styled.div`
@@ -53,7 +52,6 @@ const InnerModalDiv = styled.div`
   border: 4px solid green;
   background-color: white;
 
-
   ${media.desktop`
     padding: 2rem 0;
   `};
@@ -61,15 +59,25 @@ const InnerModalDiv = styled.div`
 
 const ModalGrid = styled.div`
   display: grid
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: 50px 50px
-  grid-gap: 5px
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  gap: 4em 1em;
+  padding-left: .5rem
+  padding-right: .5rem
 `
 
 const ModalItem = styled.div`
   display: flex
   justify-content: center
   padding: .5rem
+`
+
+const UploadDiv = styled.div`
+  display: flex
+  justify-content: center
+  padding: .5rem
+  background-color: grey
+  cursor: pointer
 `
 
 export default function ImageUploader() {
@@ -85,15 +93,17 @@ export default function ImageUploader() {
       const newImageURLs = []
       images.forEach(image => newImageURLs.push(URL.createObjectURL(image)))
       setImageURLs(newImageURLs)
+      console.log(newImageURLs)
     },
     [images]
   )
 
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
-  
+
   function onImageChange(e) {
     setHide(false)
+    handleClose()
     setImages([...e.target.files])
   }
 
@@ -118,25 +128,38 @@ export default function ImageUploader() {
         </ImageUploadDiv>
       ) : null}
 
-      <Modal
-        open={open}
-        onClose={handleClose}
-      >
+      <Modal open={open} onClose={handleClose}>
         <ModalContainerDiv>
           <InnerModalDiv>
-            <Close fontSize='large' style={{marginTop: '-1rem'}}/>
-            <Typography style={{paddingLeft: '1rem', marginTop: '1rem'}}>Add or select an image that represents your story</Typography>
+            <Close
+              fontSize="large"
+              style={{ marginTop: '-1rem', cursor: 'pointer' }}
+              onClick={handleClose}
+            />
+            <Typography
+              style={{
+                paddingLeft: '1rem',
+                marginTop: '1rem',
+                marginBottom: '3rem'
+              }}
+            >
+              Add or select an image that represents your story
+            </Typography>
             <ModalGrid>
-              <ModalItem>1</ModalItem>
-              <ModalItem>2</ModalItem>
-              <ModalItem>3</ModalItem>
-              <ModalItem>4</ModalItem>
-              <ModalItem>5</ModalItem>
-              <ModalItem>6</ModalItem>
+              <UploadDiv
+                style={{ border: '2px solid black' }}
+                onClick={() => inputRef.current.click()}
+              >
+                <FaUpload size={28} />
+              </UploadDiv>
+              <ModalItem style={{ border: '2px solid black' }}>2</ModalItem>
+              <ModalItem style={{ border: '2px solid black' }}>3</ModalItem>
+              <ModalItem style={{ border: '2px solid black' }}>4</ModalItem>
+              <ModalItem style={{ border: '2px solid black' }}>5</ModalItem>
+              <ModalItem style={{ border: '2px solid black' }}>6</ModalItem>
             </ModalGrid>
           </InnerModalDiv>
         </ModalContainerDiv>
-
       </Modal>
       {imageURLs.map(imageSrc => <img src={imageSrc} alt="testImage" />)}
     </>
