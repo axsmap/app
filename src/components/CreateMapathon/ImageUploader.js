280a0e443cc04b3f06394fec756a7e6ebf78e79a
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2022-03-05 21:32:56 -0700

    styling changes

--- src/components/CreateMapathon/ImageUploader.js
+++ src/components/CreateMapathon/ImageUploader.js
@@ -1,5 +1,40 @@
+import { AddCircle } from '@material-ui/icons'
 import React, { useEffect, useRef, useState } from 'react'
 
+import styled from 'styled-components'
+import { colors, fonts, media, fontWeight, fontSize } from '../../styles'
+
+const ImageUploadDiv = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  flex-grow: 1;
+  overflow: hidden
+  padding: 1rem 1rem;
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: #d3d3d3
+  height: 20rem
+
+  ${media.desktop`
+    padding: 2rem 0;
+  `};
+`
+const InnerUploadDiv = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  flex-grow: 1;
+  overflow: hidden
+  padding: 1rem 1rem;
+  margin-top: 4rem
+  cursor: pointer
+
+  ${media.desktop`
+    padding: 2rem 0;
+  `};
+`
 export default function ImageUploader() {
   const [images, setImages] = useState([])
   const [imageURLs, setImageURLs] = useState([])
@@ -28,9 +63,16 @@ export default function ImageUploader() {
         ref={inputRef}
         style={{ display: 'none' }}
       />
-      <button onClick={() => inputRef.current.click()} type="button">
+      {/* <button onClick={() => inputRef.current.click()} type="button">
         Upload image
-      </button>
+      </button> */}
+      <ImageUploadDiv onClick={() => inputRef.current.click()}>
+        <InnerUploadDiv>
+          <AddCircle fontSize="large" />
+          Add a photo
+        </InnerUploadDiv>
+      </ImageUploadDiv>
+
       {imageURLs.map(imageSrc => <img src={imageSrc} alt="testImage" />)}
     </>
   )
