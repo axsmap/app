f435eee5530a56d07d44b9f21b99a01ba3916123
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2022-07-05 21:35:40 -0700

    reverting imageUploader to have state

--- src/components/CreateMapathon/ImageUploader.js
+++ src/components/CreateMapathon/ImageUploader.js
@@ -1,13 +1,14 @@
 import { Modal, Box, Typography, Grid } from '@material-ui/core'
+import { red } from '@material-ui/core/colors'
 import { AddCircle, Close, CloudUpload } from '@material-ui/icons'
 import React, { useEffect, useRef, useState } from 'react'
 import { FaUpload } from 'react-icons/fa'
 import styled from 'styled-components'
-import def1 from '../../images/mapathonsDefaults/def1.jpg'
-import def2 from '../../images/mapathonsDefaults/def2.jpg'
-import def3 from '../../images/mapathonsDefaults/def3.jpg'
-import def4 from '../../images/mapathonsDefaults/def4.jpg'
-import def5 from '../../images/mapathonsDefaults/def5.jpg'
+import def1 from '../../images/mapathonsDefaults/def1.jpeg'
+import def2 from '../../images/mapathonsDefaults/def2.jpeg'
+import def3 from '../../images/mapathonsDefaults/def3.jpeg'
+import def4 from '../../images/mapathonsDefaults/def4.jpeg'
+import def5 from '../../images/mapathonsDefaults/def5.jpeg'
 import { colors, fonts, media, fontWeight, fontSize } from '../../styles'
 
 const ImageUploadDiv = styled.div`
@@ -22,6 +23,9 @@ const ImageUploadDiv = styled.div`
   margin-right: auto;
   background-color: #d3d3d3
   height: 20rem
+  border: 1px solid #d3d3d3
+  border-radius: 10px
+  margin-top: 1rem
 
   ${media.desktop`
     padding: 2rem 0;
@@ -65,16 +69,19 @@ const ModalGrid = styled.div`
   display: grid
   grid-template-columns: 1fr 1fr;
   grid-template-rows: 1fr 1fr 1fr;
-  gap: 2.5em 1em;
-  padding-left: 1rem
-  padding-right: .5rem
+  gap: 1em;
+  padding-left: 3rem
 `
 
 const ModalItem = styled.div`
   display: flex
   justify-content: center
+  width: 150px
   height: 150px
-  width: 200px
+  border: 1px solid #D3D3D3
+  border-radius: 10px
+  overflow: hidden
+  cursor: pointer
 `
 
 const UploadDiv = styled.div`
@@ -85,15 +92,23 @@ const UploadDiv = styled.div`
   cursor: pointer
   border: 1px solid #D3D3D3
   border-radius: 10px
+  width: 150px
+  height: 150px
+`
+const UploadedImageDiv = styled.div`
+  height: auto
+  width: auto
+  margin-top: 1rem
+  margin-bottom: 1rem
 `
 
 export default function ImageUploader() {
   const [images, setImages] = useState([])
   const [imageURLs, setImageURLs] = useState([])
+  const [defaultImage, setDefaultImage] = useState(0)
   const [hide, setHide] = useState(true)
   const [open, setOpen] = useState(false)
   const inputRef = useRef(null)
-  const defaultURL = []
 
   useEffect(
     () => {
@@ -114,22 +129,38 @@ export default function ImageUploader() {
     setImages([...e.target.files])
   }
 
-  const handleDefaultPick = defaultImage => {}
+  const handleDefaultPick = imageNumber => {
+    // console.log('cock')
+    setDefaultImage(imageNumber)
+    setHide(false)
+    handleClose()
+    console.log(defaultImage)
+  }
 
-  const showSelectedImage = (uploadedURLs, defaultURLinput) => {
-    if (uploadedURLs) {
+  const showSelectedImage = (uploadedURLs, defaultImage) => {
+    if (uploadedURLs.length > 0) {
+      console.log('inside uploadedURLs')
       return uploadedURLs.map(imageSrc => (
-        <img src={imageSrc} alt="testImage" />
+        <UploadedImageDiv key={imageSrc}>
+          <img
+            src={imageSrc}
+            alt="testImage"
+            style={{ maxHeight: '100%', maxWidth: '100%' }}
+          />
+        </UploadedImageDiv>
       ))
+    } else if (defaultImage > 0) {
+      console.log('inside defaultImage', defaultImage)
+      return (
+        <UploadedImageDiv>
+          <img
+            src={require(`../../images/mapathonsDefaults/def${defaultImage}.jpeg`)}
+            alt="testImage"
+            style={{ maxHeight: '100%', maxWidth: '100%' }}
+          />
+        </UploadedImageDiv>
+      )
     }
-
-    if (defaultURLinput) {
-      return defaultURLinput.map(imageSrc => (
-        <img src={imageSrc} alt="testImage" />
-      ))
-    }
-
-    return null
   }
 
   return (
@@ -173,29 +204,28 @@ export default function ImageUploader() {
             <ModalGrid>
               <UploadDiv onClick={() => inputRef.current.click()}>
                 <FaUpload size={28} />
-                <UploadDiv>Upload Image</UploadDiv>
+                <> Upload Image</>
               </UploadDiv>
-              <ModalItem onClick={handleDefaultPick(def1)}>
+              <ModalItem onClick={() => handleDefaultPick(1)}>
                 <img src={def1} alt="default1" />
               </ModalItem>
-              <ModalItem>
+              <ModalItem onClick={() => handleDefaultPick(2)}>
                 <img src={def2} alt="default2" />
               </ModalItem>
-              <ModalItem>
+              <ModalItem onClick={() => handleDefaultPick(3)}>
                 <img src={def3} alt="default3" />
               </ModalItem>
-              <ModalItem>
+              <ModalItem onClick={() => handleDefaultPick(4)}>
                 <img src={def4} alt="default4" />
               </ModalItem>
-              <ModalItem>
+              <ModalItem onClick={() => handleDefaultPick(5)}>
                 <img src={def5} alt="default5" />
               </ModalItem>
             </ModalGrid>
           </InnerModalDiv>
         </ModalContainerDiv>
       </Modal>
-      {/* {imageURLs.map(imageSrc => <img src={imageSrc} alt="testImage" />)} */}
-      {showSelectedImage(imageURLs, defaultURL)}
+      {showSelectedImage(imageURLs, defaultImage)}
     </>
   )
 }
